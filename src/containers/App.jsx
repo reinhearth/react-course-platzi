41bf7dc67b64f69785c7e74cb9734f5b750ac4d3
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-02-08 00:35:52 -0600

    adding footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/Carousel-item";
+import Footer from "../components/Footer";
 
 import "../assets/styles/App.scss";
 
@@ -16,6 +17,7 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+    <Footer />
   </div>
 );
 
