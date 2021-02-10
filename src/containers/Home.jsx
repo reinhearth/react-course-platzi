fb441b086ef3f71bb831fccf25b2c1e12ea64a46
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-02-09 20:52:30 -0600

    layout component

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/Carousel-item";
-import Footer from "../components/Footer";
 import useInitialState from "../hooks/useInitialState";
 
-import "../assets/styles/App.scss";
+
 
 const API = "http://localhost:3000/initalState";
 
@@ -15,8 +13,7 @@ const Home = () => {
   const initialState = useInitialState(API);
 
   return (
-    <div className="App">
-      <Header />
+    <>
       <Search />
       {initialState.mylist?.length > 0 && (
         <Categories title="Mi Lista">
@@ -42,9 +39,7 @@ const Home = () => {
           ))}
         </Carousel>
       </Categories>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
