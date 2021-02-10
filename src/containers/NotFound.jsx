79d035ccf10df96ecafdca6dd299166ebd8a84db
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-02-09 20:47:01 -0600

    not found component and react fragment

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => (
+  <>
+    <h1>No Encontrado</h1>
+  </>
+
+  //<React.fragment> </React.fragment> we can use this way or the short one by typing <></>
+);
+
+export default NotFound;
